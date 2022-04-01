94d43f3bf3058c1cfb010e582b251690838300b8
skuntforg <curlymacv@gmail.com>
2022-04-01 15:00:52 +0100

    added force with local minima escape. dont change!

--- Rover_Closed_Loop3.m
+++ Rover_Closed_Loop3.m
@@ -26,15 +26,15 @@ Omega_mod = zeros(1,4); % Intialise wheel rotational velocity vector
 unmatched = zeros(3,1);
 
 simtime = 0;                % Simulation timer, s
-maxtime = 60;             % Upper simulation timer limit, s
+maxtime = 40;             % Upper simulation timer limit, s
 
-dt = 0.001;                  % Simulation time segments, s=0.001
+dt = 0.01;                  % Simulation time segments, s=0.001
 i = 2;
 Kp = 2.3;                     %proportional gain    2.3
-Ki = 2.5;                      %integral gain 2.5    
+Ki = 3.5;                      %integral gain 2.5    
 Kd = 0.001;                      %derivative gain 0.001
 Kp2 = 2.1;     %2.1
-Ki2 = 0.002 ;   %            0.002
+Ki2 = 0.05 ;   %            0.002
 Kd2 = 0;           %0
 dV = 0;
 dv = 0;
@@ -50,24 +50,31 @@ deltapsi = 0;
 dvelocity = 0;
 rho = 0;
 resultant_v = 0;
-
+q = 0;
+rho_r = 0;
 %% Define the artificial potential field parameters
 % Points of attraction
 Xg = 10;  %10,1
-Yg = 1;
+Yg = 10;
 % Location of obstacles
-nobs = 1;
-xo = [5];
-yo = [1];
+nobs = 3;
+% nobs = 5;
+% xo = [10 20 30 40 50];
+% yo = [1 1 1 1 1];
+% xo = [3 5.5 8];
+% yo = [-1 0 -2];
+xo = [4.1 4.1 7]
+yo = [4 5.8 8.5]
 %Radius of obstacles (rho_0)
-rho_0 = [3];
+% rho_0 = [7 5 8 4 5];
+% rho_0 = [2 0.5 2];
+rho_0 = [1.5 1.5 0.9]
 % Constants.
-Ka = 5; 
-Kr = [1500]
-% Kry = [10];
-% Krx = [300];
+Ka = 2; 
+Kr = [10000 4000 1200]
 Kdamp = 50;
 m = 2.148;
+s = 0.1;
 vx = 0;
 vy = 0;
 vr = 0;
@@ -116,7 +123,8 @@ for simtime = 0:dt:maxtime
     % INPUT FROM CONTROLLER
     V = [dV+dv dV+dv dV-dv dV-dv]; 
     %V = [dV dV dV dV];
-    %V = [6+dv 6+dv 6-dv 6-dv];
+    %V = [5.5+dv 5.5+dv 5.5-dv 5.5-dv];
+    % V = [12 12 12 12];
    
  %% Calculate the conservative forces
     % Attractive force (Fa)
@@ -145,9 +153,18 @@ for simtime = 0:dt:maxtime
     Fd(2,1) = -Kdamp*vy;
     fprintf('Fd\n')
     disp(Fd)
+    % Added Force
+    Fadd = zeros(2,1);
+    rho_g = sqrt((xt-Xg)^2+(yt-Yg)^2);
+    if vx < 0.0001 && rho_g > 0.2
+         Fadd(1,1) = 3*0.3*(xt-Xg);
+    end 
+    if vy < 0.0001 && rho_g > 0.2
+         Fadd(2,1) = 3*0.3*(yt-Yg);
+    end
     % Total force (F)
-    Fy = Fa(2,1) + sum(Fr(2,1)) + Fd(2,1);
-    Fx = Fa(1,1) + sum(Fr(1,1)) + Fd(1,1);
+    Fy = Fa(2,1) + sum(Fr(2,1)) + Fd(2,1) + Fadd(2,1);
+    Fx = Fa(1,1) + sum(Fr(1,1)) + Fd(1,1) + Fadd(1,1);
     F(1,1) = Fx;
     F(2,1) = Fy;
     fprintf('Fx\n')
@@ -155,10 +172,17 @@ for simtime = 0:dt:maxtime
     fprintf('Fy\n')
     disp(Fy)
     % Velocity and Position (vx, vy, x, y) through integrating the equations
+    rho_g = sqrt((xt-Xg)^2+(yt-Yg)^2); %distance to goal
     vx = vx + dt*(Fx/m);
+%     if vx < 0.001 % && rho_g > 0.2
+%         vx = vx + 0.05
+%     end
     fprintf('vx\n')
     disp(vx)
     vy = vy + dt*(Fy/m);
+%     if vy < 0.001 % && rho_g > 0.2
+%         vy = vy + 0.05
+%     end
     fprintf('vy\n')
     disp(vy)
     xt = xt + dt*vx;
@@ -224,10 +248,11 @@ for simtime = 0:dt:maxtime
 %         if vx && vy < 0
 %             dvelocity = -vr;
        %end
-%         low = -1.0;
-%         high = 1.0;
+%         low = -1.4;
+%         high = 1.4;
 %         dvelocity = min(max(dvelocity, low), high);
     end
+    dvelocity = vr;
     resultant_v = sqrt(x_mod(1)^2+x_mod(2)^2);
     deltav = dvelocity - resultant_v;              %error
     intd1 = intd1 + dt*deltav;              %integral term
@@ -247,7 +272,7 @@ for simtime = 0:dt:maxtime
     %YAW controller  
     %calculate desired heading angle geometrically
     psid = atan2((yt-x_mod(8)),(xt-x_mod(7)));
-    %psid = -3;  %3.1398=179.9 0.785 ~ 45
+    %psid = 3.07;  %3.1398=179.9 0.785 ~ 45
     %calculate the difference between desired and actual heading angle
     deltapsi = psid - x_mod(12);  
     intd2 = intd2 + dt*deltapsi;
@@ -306,53 +331,90 @@ subplot(2,2,4)
 plot(xout_mod(:,8),xout_mod(:,7))
 xlabel('y_pos [m]')
 ylabel('x_pos [m]')
+set(gca,'FontSize',14)
+
 
 figure(2);
-subplot(3,1,1)
-plot(time, deltav_mod)
-xlabel('time(s)')
-ylabel('error(m/s)')
-subplot(3,1,2)
-plot(time, deltapsi_mod*180/pi)
-xlabel('time(s)')
-ylabel('error(degrees)')
-subplot(3,1,3)
-plot(time, resultant_v_mod)
-xlabel('time(s)')
-ylabel('resultant velocity m/s')
+% subplot(3,1,1)
+plot(time, deltav_mod, 'LineWidth', 2.5)
+xlabel('time(s)', 'FontSize', 16)
+ylabel('error(m/s)', 'FontSize', 16)
+set(gca,'FontSize',14)
+
+
+figure(6)
+plot(time, deltapsi_mod*180/pi, 'LineWidth', 2.5)
+ xlabel('time(s)', 'FontSize', 16)
+ ylabel('error(degrees)', 'FontSize', 16)
+set(gca,'FontSize',14)
+
 
 figure(3)
-subplot(5,1,1)
-plot(time, Fa_mod)
-xlabel('time (s)')
-ylabel('Magnitude Fa')
+plot(time, resultant_v_mod, 'LineWidth', 2.5)
+xlabel('time [s]', 'FontSize', 16)
+ylabel('resultant velocity [m/s]', 'FontSize', 16)
+set(gca,'FontSize',14)
 
-subplot(5,1,2)
-plot(time, Fr_mod)
-xlabel('Time (s)')
-ylabel('Magnitude Fr')
-subplot(5,1,3)
-plot(time, xout_mod(:,7))
+figure(4)
+plot(time,Vout(:,1), 'LineWidth', 2.5)
+xlabel('time [s]', 'FontSize', 16)
 hold on
-plot(time, xout_mod(:,8))
-xlabel('time (s)')
-ylabel('co-ordinate (m)')
-legend('X', 'Y')
-subplot(5,1,4)
-plot(time, Fx_mod)
-xlabel('time (s)')
-ylabel('Magnitude Fx')
-subplot(5,1,5)
-plot(time, Fy_mod)
-xlabel('time (s)')
-ylabel('Magnitude Fy')
+plot(time,Vout(:,2), 'LineWidth', 2.5)
+hold on
+plot(time,Vout(:,3), 'LineWidth', 2.5)
+hold on
+plot(time,Vout(:,4), 'LineWidth', 2.5)
+ylabel('Voltage of Motors [V]', 'FontSize', 16)
+legend('V1', 'V2', 'V3', 'V4')
+set(gca,'FontSize',14)
 
+figure(5)
+plot(xout_mod(:,8),xout_mod(:,7), 'LineWidth', 2.5)
+xlabel('y_pos [m]', 'FontSize', 16)
+ylabel('x_pos [m]', 'FontSize', 16)
+set(gca,'FontSize',14)
+
+% subplot(3,1,2)
+% plot(time, deltapsi_mod*180/pi)
+% xlabel('time(s)')
+% ylabel('error(degrees)')
+% subplot(3,1,3)
+% plot(time, resultant_v_mod)
+% xlabel('time(s)')
+% ylabel('resultant velocity m/s')
+% 
+% % figure(3)
+% % subplot(5,1,1)
+% % plot(time, Fa_mod)
+% % xlabel('time (s)')
+% % ylabel('Magnitude Fa')
+% % 
+% % subplot(5,1,2)
+% % plot(time, Fr_mod)
+% % xlabel('Time (s)')
+% % ylabel('Magnitude Fr')
+% % subplot(5,1,3)
+% % plot(time, xout_mod(:,7))
+% % hold on
+% % plot(time, xout_mod(:,8))
+% % xlabel('time (s)')
+% % ylabel('co-ordinate (m)')
+% % legend('X', 'Y')
+% % subplot(5,1,4)
+% % plot(time, Fx_mod)
+% % xlabel('time (s)')
+% % ylabel('Magnitude Fx')
+% % subplot(5,1,5)
+% % plot(time, Fy_mod)
+% % xlabel('time (s)')
+% % ylabel('Magnitude Fy')
+% 
 %% Display the results
 % Plot the trajectory
-figure(4);
+figure(18);
 % Now the attractive potential contour
-xmin = -20; xmax = 20;
-ymin = -20; ymax = 20;
+xmin = -10; xmax = 35;
+ymin = -10; ymax = 35;
 [Y,X] = meshgrid(xmin:1:xmax,ymin:1:ymax);
 Z = 1.0*(0.5*Ka*sqrt((X-Xg).^2+(Y-Yg).^2));
 Zmax = max(max(Z));
@@ -363,6 +425,7 @@ plot(Yg,Xg,'bx','LineWidth',10);
 xlabel('y(m)');
 ylabel('x(m)');
 axis('equal');
+set(gca,'FontSize',14)
 % Now the repulsive potential contour
 ........
 % The obstacle contours
@@ -389,12 +452,12 @@ axis equal;
 axis tight; 
 
 % Plot the theoretical trajectory
-figure(5);
+figure(15);
 plot(yt_mod, xt_mod)
 xlabel('yt (m)')
 ylabel('xt (m)')
 
-figure(6)
+figure(16)
 plot(time, vx_mod)
 hold on
 plot(time, vy_mod)
@@ -402,7 +465,7 @@ xlabel('Time (s)')
 ylabel('Theoretical Velocity (m/s)')
 legend('vx', 'vy')
 
-figure(7)
+figure(17)
 plot(xt_mod)
 legend('xt')
 
@@ -410,17 +473,30 @@ figure(8)
 plot(yt_mod)
 legend('yt')
 
-
-
+% 
+% 
 figure 
 p = 0.5*Ka*((X-Xg).^2+(Y-Yg).^2);
- rho_r = sqrt((X-xo).^2+(Y-yo).^2);
- q= 0.5*Kr*((1./rho_r-1./rho_0).^2);  %CHANGE THIS BACK TO THE RIGHT FIELD AS IN FR CALC
-mesh(X,Y,p)
- figure
- mesh(X,Y,q)
- figure
- mesh(X,Y,p+q)
+mesh(X,Y,p);
 
-
- 
+for n=1:nobs
+    
+    rho_r = sqrt((X-xo(n)).^2+(Y-yo(n)).^2);
+    q = q + 0.5*Kr(n)*((1./rho_r-1./rho_0(n)).^2);  %CHANGE THIS BACK TO THE RIGHT FIELD AS IN FR CALC
+    
+end 
+% 
+% s = 2;
+% ming = 5;
+% r = s*((X-Xg).^2+(Y-Yg).^2)+ ming;
+% 
+%  
+%  figure
+%  mesh(X,Y,q)
+%  figure
+%  mesh(X,Y,r)
+%  figure
+%  mesh(X,Y,p+q)
+% 
+% 
+%  
